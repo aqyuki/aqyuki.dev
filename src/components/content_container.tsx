1215a66b2527bb6924abe1bb1f2d7d4c2b531dac
aqyuki <yksk171118@gmail.com>
2023-12-08 21:28:21 +0900

    feat: move common content

---
+++ src/components/content_container.tsx
@@ -0,0 +1,14 @@
+export default function ContentContainer({
+  children,
+  className,
+}: {
+  children?: React.ReactNode;
+  className?: string;
+}) {
+  const class_name: string =
+    "content-container " + (className ? className : "");
+  if (children) {
+    return <div className={class_name}>{children}</div>;
+  }
+  return <></>;
+}
---
+++ src/components/content_text.tsx
@@ -0,0 +1,10 @@
+export default function ContentText({
+  children,
+}: {
+  children?: React.ReactNode;
+}) {
+  if (children) {
+    return <p className="w-body">{children}</p>;
+  }
+  return <></>;
+}

ab311609581dfde3030abdc6b8889ed2961584f9
aqyuki <yksk171118@gmail.com>
2024-01-24 05:17:04 +0900

    feat: add profile page

---
+++ src/features/profile/Profile.tsx
@@ -0,0 +1,46 @@
+import style from './profile.module.css';
+import { jost } from '@/utils/fonts/fonts';
+import Avatar from '@/features/avatar/Avatar';
+import Links from '../links/Links';
+
+export default function Profile() {
+  return (
+    <div>
+      <div className={style.profile_fix_center}>
+        <div className={style.profile_card}>
+          <Avatar />
+          <h1 className={jost.className}>Student Developer / uwaaa</h1>
+          <Links />
+        </div>
+      </div>
+
+      <div className={style.profile_fix_center}>
+        <div className={style.profile_card}>
+          <h1 className={jost.className}>About Me</h1>
+          <p className={jost.className}>
+            NITKCに所属する学生です。
+            <span> 主に、制御技術に関することについて学んでいます。</span>
+          </p>
+          <p className={jost.className}>
+            趣味はゲームをすることで、
+            <span>最近はブルーアーカイブをよくプレイしています。</span>
+          </p>
+          <p className={jost.className}>
+            また、プログラムを書くことも好きで、
+            <span>GoやTypeScriptをメインで使っています。</span>
+          </p>
+        </div>
+
+        <div className={style.profile_card}>
+          <h1 className={jost.className}>Skills</h1>
+          {/* 言語やツールのアイコンのSVGを並べたい */}
+        </div>
+
+        <div className={style.profile_card}>
+          <h1 className={jost.className}>Links</h1>
+          <Links />
+        </div>
+      </div>
+    </div>
+  );
+}

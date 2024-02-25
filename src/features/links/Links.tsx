b29caed63cf105899d40ce5e80c24e7329ed99a6
aqyuki <117277631+aqyuki@users.noreply.github.com>
2024-02-25 22:37:22 +0900

    feat: add bluesky link (#36)

--- src/features/links/Links.tsx
+++ src/features/links/Links.tsx
@@ -1,14 +1,18 @@
 import style from './links.module.css';
 import { FaGithub, FaXTwitter } from 'react-icons/fa6';
+import { PiButterflyFill } from 'react-icons/pi';
 
 export default function Links() {
   return (
     <div className={style.links}>
+      <a href="https://github.com/aqyuki">
+        <FaGithub size={30} />
+      </a>
       <a href="https://x.com/aqyuki21s">
         <FaXTwitter size={30} />
       </a>
-      <a href="https://github.com/aqyuki">
-        <FaGithub size={30} />
+      <a href="https://bsky.app/profile/aqyuki.dev">
+        <PiButterflyFill size={30} />
       </a>
     </div>
   );

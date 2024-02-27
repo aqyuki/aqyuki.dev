9e1399dfa40ed4fd4d32e08c90aad2124b43fb8c
aqyuki <uwaaa@aqyuki.dev>
2024-02-27 12:06:54 +0900

    feat: add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,39 @@
+import { Metadata } from 'next';
 import './globals.css';
+
+export const metadata: Metadata = {
+  title: 'uwaaa - Student Developer',
+  description: '学生プログラマーのポートフォリオサイトです。',
+  icons: [
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '16x16',
+      url: '/favicons/favicon-16x16.png',
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '32x32',
+      url: '/favicons/favicon-32x32.png',
+    },
+    {
+      rel: 'apple-touch-icon',
+      sizes: '180x180',
+      url: '/favicons/apple-touch-icon.png',
+    },
+    {
+      rel: 'mask-icon',
+      color: '#5bbad5',
+      url: '/favicons/safari-pinned-tab.svg',
+    },
+  ],
+  manifest: '/favicons/site.webmanifest',
+  other: {
+    'msapplication-TileColor': '#da532c',
+    'theme-color': '#ffffff',
+  },
+};
 
 export default function RootLayout({
   children,
@@ -7,11 +42,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <title>uwaaa - Student Developer</title>
-      <meta
-        name="description"
-        content="学生プログラマーのポートフォリオサイトです。"
-      />
       <body>{children}</body>
     </html>
   );
